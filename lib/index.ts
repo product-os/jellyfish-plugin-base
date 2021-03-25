3bee34dbd88d2855ce5c3ba7b6a0698db6450897
balena-ci <balena-ci@users.noreply.github.com>
2021-03-25 05:36:45 +0000

    deploy: 35dfbbac2999f11f76fad3ec0a553b3fc32a4c79

--- lib/index.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (C) Balena.io - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited.
- * Proprietary and confidential.
- */
-
-export { JellyfishPluginBase } from './jellyfish-plugin';
-export { PluginManager } from './plugin-manager';
-export * from './types';
